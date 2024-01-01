178b947317bbc262c624ca485ace24c02751af72
hakouguelfen <hakoudev@gmail.com>
2024-01-01 18:38:26 +0100

    make neovim transparent

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,3 +2,12 @@ require "config"
 
 pcall(vim.cmd, 'colorscheme base16-onedark')
 pcall(vim.cmd, 'highlight Normal guibg=none')
+
+
+-- Enable transparency
+pcall(vim.cmd, [[ hi Normal guibg=NONE ctermbg=NONE ]])
+pcall(vim.cmd, [[ hi NonText guibg=NONE ctermbg=NONE ]])
+pcall(vim.cmd, [[ hi LineNr guibg=NONE ctermbg=NONE ]])
+pcall(vim.cmd, [[ hi VertSplit guibg=NONE ctermbg=NONE ]])
+pcall(vim.cmd, [[ hi StatusLine guibg=NONE ctermbg=NONE ]])
+pcall(vim.cmd, [[ hi StatusLineNC guibg=NONE ctermbg=NONE ]])
