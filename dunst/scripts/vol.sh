979dbdf858ed1a1f59e1a49ac367fefb04d0fe74
Hakou Guelfen <hakoudev@gmail.com>
2025-04-27 01:29:15 +0100

    Fix some outdated configs

--- dunst/scripts/vol.sh
+++ dunst/scripts/vol.sh
@@ -10,7 +10,7 @@ mute="$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $NF}')"
 if [[ $volume == 0 || "$mute" == "[MUTED]" ]]; then
     dunstify -a "$scriptName" -t 1500 -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag "Volume muted"
 else
-    dunstify -a "$scriptName" -t 1500 -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume" "Volume: ${volume}%" --icon=~/.local/share/icons/Colloid-grey/actions/16/player-volume.svg
+    dunstify -a "$scriptName" -t 1500 -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume" "Volume: ${volume}%" --icon=~/.local/share/icons/Colloid-Grey-Nord/actions/16/player-volume.svg
 fi
 
 pkill -RTMIN+10 dwmblocks
