ebbaa80c3d40de80df23e0e6bac3691a9e5a6f42
Hakou Guelfen <hakoudev@gmail.com>
2025-04-28 19:45:18 +0100

    Rewrite: remove default values from configuration

--- dunst/scripts/toggle.sh
+++ dunst/scripts/toggle.sh
@@ -1,16 +1,16 @@
 #!/bin/env bash
 
-msgTag="myvolume"
-scriptName="volController"
+tag="myvolume"
+scriptName="Volume Controller"
 
 wpctl set-mute @DEFAULT_SINK@ toggle
 volume="$(wpctl get-volume @DEFAULT_SINK@ | awk '{print $NF * 100}')"
 mute="$(wpctl get-volume @DEFAULT_SINK@ | awk '{print $NF}')"
 
 if [[ $volume == 0 || "$mute" == "[MUTED]" ]]; then
-    dunstify -a "$scriptName" -t 1500 -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag "Volume muted"
+    dunstify -a "$scriptName" -t 1500 -i custom/volume-muted -h string:x-dunst-stack-tag:$tag "Volume muted"
 else
-    dunstify -a "$scriptName" -t 1500 -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume" "Volume: ${volume}%" --icon=~/.local/share/icons/Colloid-Grey-Nord/actions/16/player-volume.svg
+    dunstify -a "$scriptName" -t 1500 -i custom/volume-high -h string:x-dunst-stack-tag:$tag "Volume: " -h int:value:"$volume" 
 fi
 
 pkill -RTMIN+10 dwmblocks
--- dunst/scripts/vol.sh
+++ dunst/scripts/vol.sh
@@ -1,17 +1,16 @@
 #!/bin/env bash
 
-msgTag="myvolume"
-scriptName="volController"
+tag="myvolume"
+scriptName="Volume Controller"
 
 wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ "$@"
 volume="$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $NF * 100}')"
 mute="$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $NF}')"
 
 if [[ $volume == 0 || "$mute" == "[MUTED]" ]]; then
-    dunstify -a "$scriptName" -t 1500 -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag "Volume muted"
+    dunstify -a "$scriptName" -t 1500 -i custom/volume-muted -h string:x-dunst-stack-tag:$tag "Volume muted"
 else
-    dunstify -a "$scriptName" -t 1500 -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume" "Volume: ${volume}%" --icon=~/.local/share/icons/Colloid-Grey-Nord/actions/16/player-volume.svg
+    dunstify -a "$scriptName" -t 1500 -i custom/volume-high -h string:x-dunst-stack-tag:$tag "Volume: " -h int:value:"$volume"
 fi
 
 pkill -RTMIN+10 dwmblocks
-#canberra-gtk-play -i audio-volume-change -d "$scriptName"
